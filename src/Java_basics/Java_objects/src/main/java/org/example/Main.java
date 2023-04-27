268888233b13c66a4cd9d06a040294094215ebbe
muxa1508 <muxa1508@inbox.ru>
2023-04-27 11:51:27 +0300

    Completed homework 5: Java objects

---
+++ src/Java_basics/Java_objects/src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String maxName = null;
+
+
+    public static void main(String[] args) {
+        check();
+    }
+
+    static String input() {
+        return scanner.nextLine();
+    }
+
+    static void check() {
+        while (true) {
+            System.out.println("Введите имя с большой буквы или end для завершения программы:");
+            String name = input();
+            if (name.equals("end")) {
+                System.out.println("Программа завершена");
+                break;
+            }
+            if (Character.isUpperCase(name.charAt(0))) {
+                if (maxName == null) {
+                    maxName = name;
+                    System.out.println("Это первое введенное имя!");
+                } else if (maxName.length() < name.length()) {
+                    maxName = name;
+                } else if (maxName.length() > name.length()) {
+                    System.out.println("Не хватило " + (maxName.length() - name.length()) + " символов");
+                }
+            } else {
+                System.out.println("Имена нужно вводить с большой буквы!");
+            }
+        }
+        if (maxName != null) {
+            System.out.println("Самое длинное имя: " + maxName);
+        }
+    }
+}
