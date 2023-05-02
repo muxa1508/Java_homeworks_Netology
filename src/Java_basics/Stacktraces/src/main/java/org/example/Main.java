123bfbfd6d29fe903ea369e2efbd7a0429e4fda1
muxa1508 <muxa1508@inbox.ru>
2023-05-02 17:43:11 +0300

    Completed homework 12: Stacktrace

--- src/Java_basics/Stacktraces/src/main/java/org/example/Main.java
+++ src/Java_basics/Stacktraces/src/main/java/org/example/Main.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+    protected static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        PasswordChecker passwordChecker = new PasswordChecker();
+
+        System.out.println("Введите мин. длину пароля: ");
+        passwordChecker.setMinLength(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println("Введите макс. допустимое количество символов подряд");
+        passwordChecker.setMaxRepeats(Integer.parseInt(scanner.nextLine()));
+
+        while (true) {
+            System.out.println("Введите пароль или end");
+            String pass = scanner.nextLine();
+            if (endCheck(pass)) {
+                break;
+            }
+            if (passwordChecker.verify(pass)) {
+                System.out.println("Подходит !" + "\n");
+            } else {
+                System.out.println("Не подходит !" + "\n");
+            }
+        }
+    }
+
+    protected static boolean endCheck(String input) {
+        if (input.equals("end")) {
+            System.out.println("Программа завершена");
+            return true;
+        }
+        return false;
     }
 }
--- src/Java_basics/Stacktraces/src/main/java/org/example/PasswordChecker.java
+++ src/Java_basics/Stacktraces/src/main/java/org/example/PasswordChecker.java
@@ -10,7 +10,7 @@ public class PasswordChecker {
 
     public void setMinLength(int value) {
         if (value < 1) {
-            throw new IllegalArgumentException("Значение не может быть отрицательным");
+            throw new IllegalArgumentException("Значение не может быть отрицательным или равно нулю");
         } else {
             minLength = value;
         }
@@ -27,7 +27,25 @@ public class PasswordChecker {
     public boolean verify(String password) {
         if (minLength == 0 || maxRepeats == 0) {
             throw new IllegalStateException("Не заданы начальные параметры проверки");
+        } else {
+            char[] inputChar = password.toCharArray();
+            if (inputChar.length < minLength) {
+                return false;
+            }
+            char lastChar = inputChar[0];
+            int repeatsCount = 1;
+            for (int i = 1; i < inputChar.length; i++) {
+                if (inputChar[i] == lastChar) {
+                    repeatsCount++;
+                    if (repeatsCount > maxRepeats) {
+                        return false;
+                    }
+                } else {
+                    lastChar = inputChar[i];
+                    repeatsCount = 1;
+                }
+            }
         }
-
+        return true;
     }
 }
