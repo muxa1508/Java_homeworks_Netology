0dc5610dfb82fc02b41051758732f4f631e15121
muxa1508 <muxa1508@inbox.ru>
2023-06-28 16:05:06 +0300

    Homework 6: HashMap First task of homework completed

--- src/Git_and_java_basics/HashMap/src/main/java/org/example/Main.java
+++ src/Git_and_java_basics/HashMap/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class Main {
+
+    static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \n" +
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \n" +
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \n" +
+            "nisi ut aliquip ex ea commodo consequat. \n" +
+            "Duis aute irure dolor in reprehenderit in voluptate velit \n" +
+            "esse cillum dolore eu fugiat nulla pariatur. \n" +
+            "Excepteur sint occaecat cupidatat non proident, \n" +
+            "sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Task1 task1 = new Task1();
+        task1.charCheck(text);
+
+
     }
 }
---
+++ src/Git_and_java_basics/HashMap/src/main/java/org/example/Task1.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task1 {
+
+    public void charCheck(String text) {
+
+        Map<Character, Integer> charMap = new HashMap<>();
+
+        for (int i = 0; i < text.length(); i++) {
+            Character iChar = text.toLowerCase().charAt(i);
+            if (Character.isLetter(iChar)) {
+                if (charMap.containsKey(iChar)) {
+                    charMap.put(iChar, charMap.get(iChar) + 1);
+                } else {
+                    charMap.put(iChar, 1);
+                }
+            }
+        }
+//        charMap.forEach((key, value) -> System.out.println(key + " " + value));
+
+        int maxIntValue = -1;
+        char maxCharValue = 0;
+        int minIntValue = Integer.MAX_VALUE;
+        char minCharValue = 0;
+
+        for (Character key : charMap.keySet()) {
+            if (charMap.get(key) > maxIntValue) {
+                maxIntValue = charMap.get(key);
+                maxCharValue = key;
+            }
+            if (charMap.get(key) < minIntValue) {
+                minIntValue = charMap.get(key);
+                minCharValue = key;
+            }
+        }
+
+        System.out.println("Наиболее часто встречающаяся буква: " + maxCharValue +
+                " встречается " + maxIntValue + " раз" + "\n" +
+                "Наименее часто встречающаяся буква: " + minCharValue +
+                " встречается " + minIntValue + " раз");
+    }
+}
+
