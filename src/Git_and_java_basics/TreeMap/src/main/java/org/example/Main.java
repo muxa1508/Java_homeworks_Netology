6932e515cf621758dc979943773114459b267aff
muxa1508 <muxa1508@inbox.ru>
2023-07-07 13:13:21 +0300

    Homework 7: TreeMap Homework completed

--- src/Git_and_java_basics/TreeMap/src/main/java/org/example/Main.java
+++ src/Git_and_java_basics/TreeMap/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Person> list = new ArrayList<>();
+
+        list.add(new Person("Алексей","Холодков",10));
+        list.add(new Person("Алексей","Холодков",30));
+        list.add(new Person("Михаил", "Салтыков-Щедрин", 10));
+        list.add(new Person("Михаил", "Салтыков-Щедрин", 40));
+        list.add(new Person("Михаил", "Салтыков-Щедрин", 10));
+        list.add(new Person("Чингачгук", "Вождь племени краснокожих", 4));
+        list.add(new Person("Чингачгук", "Вождь племени краснокожих", 15));
+        System.out.println(list);
+
+
+        Collections.sort(list, new PersonComparator(2));
+
+        System.out.println(list);
     }
 }
