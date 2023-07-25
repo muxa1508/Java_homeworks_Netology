405f5ef2ca6d6c6a41d1249a13436ac52d9945e8
muxa1508 <muxa1508@inbox.ru>
2023-07-25 17:06:03 +0300

    Homework 1: Lambda. Task 1 complete

--- src/Java_core/Lamda/src/main/java/org/example/Main.java
+++ src/Java_core/Lamda/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -10,8 +11,8 @@ public class Main {
 
         List<Person> list = new ArrayList<>();
 
-        list.add(new Person("Алексей","Холодков",10));
-        list.add(new Person("Алексей","Холодков",30));
+        list.add(new Person("Алексей", "Холодков", 10));
+        list.add(new Person("Алексей", "Холодков", 30));
         list.add(new Person("Михаил", "Салтыков-Щедрин", 10));
         list.add(new Person("Михаил", "Салтыков-Щедрин", 40));
         list.add(new Person("Михаил", "Салтыков-Щедрин", 10));
@@ -20,7 +21,35 @@ public class Main {
         System.out.println(list);
 
 
-        Collections.sort(list, new PersonComparator(2));
+        list.sort(new Comparator<Person>() {
+
+            int maxSurnameLenght = 2;
+
+            @Override
+            public int compare(Person o1, Person o2) {
+                int o1SurnameLenght = o1.getSurname().split("(?U)\\W").length;
+                int o2SurnameLenght = o2.getSurname().split("(?U)\\W").length;
+
+                if ((o1SurnameLenght & o2SurnameLenght) >= maxSurnameLenght) {
+                    if (o1.getAge() < o2.getAge()) {
+                        return 1;
+                    } else if (o1.getAge() == o2.getAge()) {
+                        return 0;
+                    }
+                } else {
+                    if (o1SurnameLenght < o2SurnameLenght) {
+                        return 1;
+                    } else if (o1SurnameLenght == o2SurnameLenght) {
+                        if (o1.getAge() < o2.getAge()) {
+                            return 1;
+                        } else if (o1.getAge() == o2.getAge()) {
+                            return 0;
+                        }
+                    }
+                }
+                return -1;
+            }
+        });
 
         System.out.println(list);
     }
