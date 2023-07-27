475ef609e8d1b7bfae54687a02fdad11715ffa39
muxa1508 <muxa1508@inbox.ru>
2023-07-27 15:27:25 +0300

    Homework 4: Maven_gradle complete

---
+++ src/Java_core/Maven_gradle/Gradle/db-gradle/src/main/java/org/example/DbSetting.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
